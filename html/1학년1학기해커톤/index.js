646598be5c902774c5f2eec7e6ae7fe54f955e1e
hyunho <kimhyunhi1@gmail.com>
2024-07-18 16:35:06 +0900

    c34

---
+++ html/1학년1학기해커톤/index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const boxes = document.querySelectorAll('.box');
+
+    window.addEventListener('scroll', checkBoxes);
+
+    function checkBoxes() {
+        const triggerBottom = window.innerHeight / 5 * 4;
+
+        boxes.forEach(box => {
+            const boxTop = box.getBoundingClientRect().top;
+
+            if (boxTop < triggerBottom) {
+                box.classList.add('show');
+            } else {
+                box.classList.remove('show');
+            }
+        });
+    }
+    checkBoxes();
+});
+
+function go(){
+    location.href='login.html'
+}
