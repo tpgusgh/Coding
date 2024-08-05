2fbd35df1f7f10c960c56d753b8fd8b592c7fa42
hyunho <kimhyunhi1@gmail.com>
2024-08-05 21:06:30 +0900

    11

---
+++ c\c++/반복 출력하기 1.c
@@ -0,0 +1,24 @@
+/*
+
+print 단어를 연속으로 50번 출력하시오.(띄어쓰기 없음)
+
+printprintprint...print (50번 출력)
+
+
+
+입력
+없음
+
+출력
+print (50번출력)
+*/
+
+
+
+#include<stdio.h>
+int main(){
+    for(int i=1; i<=50; i++){
+        printf("print");
+    }
+    return 0;
+}
