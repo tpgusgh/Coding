e3b1085ab493d78c4fafb68fcde30f6910a9ee1e
hyunho <kimhyunhi1@gmail.com>
2024-11-04 11:03:30 +0900

    95

---
+++ java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        study stud = new study();
+        int [] p = {10,20,30,40,50,60,70,80,90,100};
+        for(int i=0;i<p.length;i++){
+            System.out.println(p[i]);
+        }
+    }
+}
