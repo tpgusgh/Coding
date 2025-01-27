7aa36d21feca72c227af27f67b91d1ddeb287ef5
hyunho <kimhyunhi1@gmail.com>
2025-01-27 19:22:06 +0900

    dat

---
+++ Dart/for.dart
@@ -0,0 +1,11 @@
+void main(){
+    int total = 0;
+    List<int> numbers = [1,2,3,4,5,6];
+
+    for(int = 0; i < numbers.length; i++){
+        total += numbers[i];
+    }
+    
+
+    print(total);
+}
