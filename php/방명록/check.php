c5941d88dc23d9f3b3f16778f085a944015244a9
hyunho <kimhyunhi1@gmail.com>
2025-03-02 23:53:40 +0900

    PHP

--- php/방명록/check.php
+++ php/방명록/check.php
@@ -1,7 +1,7 @@
 <?php
     $conn = mysqli_connect('localhost', 'root', '', 'mydb');
     if(mysqli_connect_errno()){
-        echo 'MYSQL 접속 실패 : ' .mysqli_connect_error();
+        echo '왜지.. MYSQL 접속 실패 : ' .mysqli_connect_error();
         exit();
     }
 ?>
