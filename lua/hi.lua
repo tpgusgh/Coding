d0d6e8a440176acb63fc9f78a2f25b10e851bde4
hyunho <kimhyunhi1@gmail.com>
2025-05-12 11:04:34 +0900

    add: lua

---
+++ lua/hi.lua
@@ -0,0 +1,15 @@
+print("What is local")
+
+a=3 
+
+print(a) // 3 
+
+if true then 
+  local a = 20 
+  local b = "bbbbb"
+  print (a) // 20
+  print (b) // bbbb 
+end
+
+print (a) // 3
+print (b) // nil
